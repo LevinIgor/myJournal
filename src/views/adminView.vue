8f32f3793b25828b5405f70522246d4ec88ffc08
Ihor <i.v.levin@student.khai.edu>
2022-07-08 17:37:57 +0300

    Фильтрация поиска

--- src/views/adminView.vue
+++ src/views/adminView.vue
@@ -1,28 +1,36 @@
 <template>
   <div class="wrapper">
     <div class="admin">
-      <div class="header"><input type="text" class="search" />
-      <img src="@/assets/icons/create.png" alt="create post" class="create-icon" @click="goToPostCreate()">
+      <div class="header">
+        <input type="text" class="search" v-model="search" />
+        <img
+          src="@/assets/icons/create.png"
+          alt="create post"
+          class="create-icon"
+          @click="goToPostCreate()"
+        />
       </div>
       <div class="content">
-        <div class="post" v-for="post in posts">
-          <div class="post-title">{{ post.title }}</div>
-          <div class="post-id">{{ post.id }}</div>
-          <div class="post-control">
-            <img
-              src="@/assets/icons/edit.png"
-              alt="edit"
-              class="icon"
-              @click="goToPostEdit(post.id)"
-            />
-            <img
-              src="@/assets/icons/delete.png"
-              alt="delete"
-              class="icon"
-              @click="deletePost(post.id)"
-            />
+        <TransitionGroup name="list">
+          <div class="post" v-for="post in filterPosts">
+            <div class="post-title">{{ post.title }}</div>
+            <div class="post-id">{{ post.id }}</div>
+            <div class="post-control">
+              <img
+                src="@/assets/icons/edit.png"
+                alt="edit"
+                class="icon"
+                @click="goToPostEdit(post.id)"
+              />
+              <img
+                src="@/assets/icons/delete.png"
+                alt="delete"
+                class="icon"
+                @click="deletePost(post.id)"
+              />
+            </div>
           </div>
-        </div>
+        </TransitionGroup>
       </div>
     </div>
   </div>
@@ -30,9 +38,10 @@
 <script setup>
 import getPosts from "../firebase/getPosts";
 import deletePostBD from "../firebase/deletePost";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, computed } from "vue";
 import { useRouter } from "vue-router";
 
+let search = ref("");
 const posts = ref([]);
 const router = useRouter();
 
@@ -49,6 +58,12 @@ const deletePost = async (id) => {
     posts.value = posts.value.filter((post) => post.id !== id);
   }
 };
+
+const filterPosts = computed(() => {
+  return posts.value.filter((post) => {
+    return post.title.toLowerCase().includes(search.value.toLowerCase());
+  });
+});
 
 onMounted(async () => {
   posts.value = await getPosts();
@@ -67,19 +82,19 @@ onMounted(async () => {
   padding: 20px;
   background-color: rgb(255, 255, 255);
 }
-.header{
+.header {
   display: flex;
   align-items: center;
   justify-content: space-between;
 }
-.create-icon{
+.create-icon {
   cursor: pointer;
   width: 30px !important;
   height: 30px;
   filter: invert(50%);
   transition: filter 0.3s ease-in-out;
 }
-.create-icon:hover{
+.create-icon:hover {
   filter: invert(0%);
 }
 
@@ -140,5 +155,23 @@ onMounted(async () => {
 }
 .no {
   border: 1px solid rgb(177, 33, 33);
+}
+
+.list-move, /* apply transition to moving elements */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.list-leave-active {
+  position: absolute;
 }
 </style>
